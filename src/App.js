5443f3b0ef0477b84ea42f8ccf90269913e9ac8e
Thikshna SG <thikshna.sg@gmail.com>
2022-02-08 10:54:27 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import TableData from './comp/TableData';
+import "./App.css";
+import { useState } from "react";
+import RegistrationForm from "./comp/RegistrationForm";
 
 function App() {
+  const initialValues = {
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  };
+  const [data, setData] = useState(initialValues);
+  const [errors, setErrors] = useState({});
   return (
     <div className="App">
-     <TableData/>
+      <RegistrationForm
+        data={data}
+        setData={setData}
+        errors={errors}
+        setErrors={setErrors}
+      />
     </div>
   );
 }
